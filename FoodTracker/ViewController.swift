5af36a2d9692eaa77d43ed7eb2198f514db6ebc2
leozdgao <gzdleo0816@gmail.com>
2015-12-22 17:01:09 +0800

    finish lesson 4

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,79 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: Properties
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var mealNameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    // MARK: Lifecycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 为输入框添加delegate
+        self.nameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // 隐藏输入用键盘
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        let userInputText = textField.text!
+        // 检查输入是否为空
+        if !userInputText.isEmpty {
+            // 将输入的值作为Label的Text
+            self.mealNameLabel.text = textField.text
+            // 重置输入框
+            textField.text = ""
+        }
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        // 改变Image Viewer的图片
+        let selectedImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.photoImageView.image = selectedImage
+        
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    // MARK: Actions
+    
+    @IBAction func selectPhotoFromPhotoLibrary(sender: UITapGestureRecognizer) {
+        // 这里不需要出现键盘
+        self.nameTextField.resignFirstResponder()
+        
+        let imagePickerController = UIImagePickerController()
+        imagePickerController.sourceType = .PhotoLibrary
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func setDefaultLabelText(sender: UIButton) {
+        self.mealNameLabel.text = "Default Text"
+    }
 
 }
 
